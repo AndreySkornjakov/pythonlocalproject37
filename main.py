c663a9cf61cdcc462381de596a9a41f5c7e22a9d
PyCur <anannas@yandex.ru>
2023-10-04 19:48:33 +0300

    Проинициализировали исходные данные

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from random import randint
 
 
 def main():
-    ...
+    a = [randint(1, 9) for _ in range(5)]
+    b = [randint(1, 9) for _ in range(5)]
+
 
 if __name__ == "__main__":
     main()
