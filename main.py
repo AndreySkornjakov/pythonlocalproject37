9859607fe4598e052334d8455f1935e24f8a2dc7
PyCur <anannas@yandex.ru>
2023-10-04 19:56:17 +0300

    Реализовали функцию func

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ def main():
     a = [randint(1, 9) for _ in range(5)]
     b = [randint(1, 9) for _ in range(5)]
 
+    c = func(a, b)
+    print()
+
 
 def func(a: list, b: list) -> list:
     """"""
-    ...
+    c = []
+    for elem in a:
+        if elem not in b:
+            c.append(elem)
+    return c
+
 
 if __name__ == "__main__":
     main()
